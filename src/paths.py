683d887a1506aa06289704a17f7a98cc71c71ce0
dperezflores <daniel.apf99@gmail.com>
2026-09-17 23:10:58 -0600

    Agregar ruta del Excel fuente

---
+++ src/paths.py
@@ -0,0 +1,8 @@
+"""Rutas utilizadas por el prototipo en Google Colab."""
+
+DRIVE_MOUNT_POINT = "/content/drive"
+
+SOURCE_EXCEL_PATH = (
+    "/content/drive/MyDrive/Colab Notebooks/PROGRAMAS_2026/"
+    "INSPECCION_FISICA/INSPECCION_1_MLEO_AIN.xlsx"
+)
